6e35a03b113de8e3450e5dfcc8defd1eece689eb
Devnaagar <sunnynaagar773@gmail.com>
2024-07-18 01:12:06 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import {Sugar} from 'react-preloaders';
 
 function App() {
-
-  return (
-    <>
+  const [loading,setLoading]= useState(true);
+  useEffect(()=>{
+    const loadingdiv=()=>{
+      setTimeout(()=>{
+        setLoading(false);
+      },4500);
+    };
+    loadingdiv();
+  },[])
+  return loading?
+    (<Sugar time={4000} animation="slide-up" background="blur" color="white"/>
+    ):(
       <div>
         <h1 className='m-3'>hello world</h1>
         <i className="fa-solid fa-house"></i>
       </div>
-    </>
-  )
+    );
 }
 
 export default App
