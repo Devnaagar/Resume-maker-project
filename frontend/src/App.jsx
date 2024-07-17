b3f04179a02f5ec7adbf464d4b0eae853dd60599
Devnaagar <sunnynaagar773@gmail.com>
2024-07-17 20:43:36 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
       <div>
-        <h1 className='mx-3'>hello world</h1>
+        <h1 className='m-3'>hello world</h1>
         <i className="fa-solid fa-house"></i>
       </div>
     </>
